b4da4a0d6c647a042f01f4580c2c0afb082fbe82
workeuser <liquidum@gmail.com>
2016-02-23 16:51:03 +0500

    new app

--- tutorial/vacancy/models.py
+++ tutorial/vacancy/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User, Group
 
-# Create your models here.
+
+class Joke(models.Model):
+    date_add = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(verbose_name="Joke")
+    funny = models.BooleanField(default=False)
+    user = models.ForeignKey(User)
+
+
+class VoteUp(models.Model):
+    date_add = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User)
+    joke = models.ForeignKey(Joke)
+
+    class Meta:
+        unique_together = ('user', 'joke')
+
+
+class ExtendedGroupDescriptionForExampleHowToUseRestApi(Group):
+    description = models.TextField()
